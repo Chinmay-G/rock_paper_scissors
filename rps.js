cbf5d16a01dd0ef25b4a947a1b62b6545777e4cf
Chinmay-G <chinmay.the1@gmail.com>
2024-09-03 12:38:11 +0530

    Build play game logic
    Build 2 functions
    1. To play a round of game and increase the score accordingly
    2. Repeat the rounds 5 times to make it a best of 5 game, and display the winner

--- rps.js
+++ rps.js
@@ -20,3 +20,59 @@ function getHumanChoice() {
     return human;
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,compChoice) {
+    if(humanChoice==='ROCK' && compChoice==='SCISSORS'){
+        humanScore++;
+        return 'You Win! Rock beats Scissors.';
+    }
+    else if(humanChoice==='ROCK' && compChoice==='PAPER'){
+        computerScore++;
+        return 'Computer Wins! Paper beats Rock.';
+    }
+    else if(humanChoice==='PAPER' && compChoice==='SCISSORS'){
+        computerScore++;
+        return 'Computer Wins! Scissors beats Paper.';
+    }
+    else if(humanChoice==='PAPER' && compChoice==='ROCK'){
+        humanScore++;
+        return 'You Win! Paper beats Rock.';
+    }
+    else if(humanChoice==='SCISSORS' && compChoice==='ROCK'){
+        computerScore++;
+        return 'Computer Wins! Rock beats Scissors.';
+    }
+    else if(humanChoice==='SCISSORS' && compChoice==='PAPER'){
+        humanScore++;
+        return 'You Win! Scissors beats Paper.';
+    }
+    else if(humanChoice === compChoice){
+        return 'Round Draw';
+    }
+    else
+        return 'Invalid input';
+}
+
+
+function playGame() {
+    for(i=1; i<=5; i++) {
+        console.log(`\nRound ${i}`);
+
+        let humanSelect = getHumanChoice();
+        let compSelect = getCompChoice();
+        console.log(`You: ${humanSelect}\nComputer: ${compSelect}`);
+        console.log(playRound(humanSelect, compSelect), `| Your score: ${humanScore}, Computer score: ${computerScore}`);
+    }
+    console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
+    if(humanScore>computerScore)
+        console.log("You win!");
+    else if(computerScore>humanScore)
+        console.log("Computer Wins!\nBetter luck next time!");
+    else if(humanScore===computerScore)
+        console.log("Game Draw!");
+}
+
+console.log(`Best of 5 rounds`);
+playGame();
